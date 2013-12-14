fc87c6e99809400c06e8fbe31a96798ed48fa276
mmy <ymm.812@gmail.com>
2013-12-14 14:16:42 +0800

    Initial Commit

---
+++ Props/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Props
+//
+//  Created by mmy on 12/14/13.
+//  Copyright (c) 2013 mmy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
